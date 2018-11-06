9d097082b8f213dc8d4bd656a312b6fe1ea43a33
stefanos Sophocleous <Stefanos3sophocleous@gmail..com>
2018-11-05 20:32:40 -0800

    dayOne done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
+// var firstNameOfStefanosSopshocleous = "Ben";
+// I am a COmment Trololol
 
+
+console.log(name);
+function() {
+  console.log('sdfljkdsf');
+  console.log('sdfsdf');
+
+  if (true)
+}
