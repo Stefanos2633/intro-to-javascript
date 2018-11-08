49addd76f3d49a874af806de09da7e33538d30fd
stefanos Sophocleous <Stefanos3sophocleous@gmail..com>
2018-11-07 20:11:49 -0800

    dayTwo Java done

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,74 @@
+// var name ="Stefanos"
+//
+// var nameTwo = new String("Ben");
+//
+// var luckyNumber = 42;
+//
+// var boolean = true;
+//
+// var mystery;
+//
+// console.log(name = nameTwo);
 
+//
+// let name;
+//
+// console.log(name);
+//
+// {
+//   const name= "Stefanos";
+//   const name = "Artemis";
+//   console.log(name);
+//
+// }
+//
+// console.log(name);
+//
+// const greeting = "Hello my name is: ";
+// const name = "Bob";
+// console.log(greeting + name + '.');
+
+// const name ="Bob";
+//
+// console.log(`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione impedit, corporis autem consequatur omnis officia fuga hic, ${name} provident quaerat commodi Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione impedit, corporis autem consequatur omnis officia fuga hic, provident quaerat commodi`)
+//
+// const date = new Date('09/27/89');
+//
+// console.log(date);
+//
+// const str ='adsfjlasdflasldjkfaklsdf'
+// console.log(str.length);
+//
+//
+// const num = new Number(42);
+// const num2 = 42;
+//
+// const array = [1, 2, 3];
+// const array2 = new Array(1, 2, 3);
+// const arrayMixed = ['Stefanos', 18, true]
+//
+// console.log(arrayMixed);
+//
+// const obj = {
+//   name: 'Stefanos',
+//   age: 18
+// };
+// console.log(obj);
+//
+// alert('Danger Will Robinson!!!');
+ // const response = confirm('Are you sure?');
+ //
+ // console.log(response);
+
+// const favNum = prompt('What is your favorite movie');
+//
+// const realNum = parseInt(favNum);
+//
+// const num = '55';
+//
+// console.log(realNum);
+//
+// // number to string
+// console.log(num.toString());
+//
+// console.log(parseInt(num));
