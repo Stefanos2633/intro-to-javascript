e9b0108e18747c76fe7a378385d251eafb4fb4ee
stefanos Sophocleous <Stefanos3sophocleous@gmail..com>
2018-11-07 17:32:14 -0800

    dayOneTakeHome

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
-// var firstNameOfStefanosSopshocleous = "Ben";
-// I am a COmment Trololol
+ // var firstNameOfStefanosSopshocleous = "Ben";
+ // I am a COmment Trololol
 
 
-console.log(name);
-function() {
-  console.log('sdfljkdsf');
-  console.log('sdfsdf');
+// console.log(Stefanos Sophocleous);
+//  function() {
+//    console.log('sdfljkdsf');
+//  console.log('sdfsdf');
+// Var about
+// console.
 
-  if (true)
-}
+var aboutMe ="What i want to get out of the class is the ability to understand and type javascript to the best of my ability. I also plan to turn this hobby of mine into a career."
+var funFact =" I am super excited to take this class more seriously than most and put in the extra hours to be successfull now unlike some of my passions in the past."
+var total = aboutMe + funFact;
+console.log(total);
